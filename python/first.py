30a56c02dd8ecdbbe0cf80d18c97618a4e9cece1
KeyurAkbari007 <101794502+KeyurAkbari007@users.noreply.github.com>
2022-12-04 18:16:43 +0530

    Add files via upload

---
+++ python/first.py
@@ -0,0 +1,27 @@
+print("keyur patel", "patel", sep=",", end="\n")
+# single line comments
+"""
+ multyline 
+comments
+"""
+
+#  this is escape sequence character
+print("keyur")
+
+
+# this is variable
+
+var1 = "hello world"
+var2 = 16.5
+print(var1)
+print(type(var1))
+print(type(var2))
+# print(var2+var1)  # error var2 is flaot
+
+print(10*"keyur\n")  # this is  print 10 time only string
+
+
+print("enter your number")
+inputt = input()
+print("you enterd value is  : ", inputt)
+print(type(inputt))  # defalut type is str
